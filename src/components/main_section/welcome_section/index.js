fcc9271fae81b8a2303150c8a5fe337dcda4a0f4
fabiokleis <fabiohkrc@gmail.com>
2021-06-06 19:02:05 -0300

    rewrite the -A whoami animation to use only css keyframes

--- src/components/main_section/welcome_section/index.js
+++ src/components/main_section/welcome_section/index.js
@@ -1,37 +1,24 @@
-import React,{useState, useEffect} from 'react';
+import React  from 'react';
 import './index.css';
 
 export default function WelcomeSection(props){
-    const [whoami, setWhoami] = useState('');
-    useEffect(() => {
-        setWhoami('');
-        const frase = '$_ whoami';
-        const interval = setInterval(gen => {
-            const {value, done} = gen.next();
-            if(done){
-                clearInterval(interval);
-            }else{
-                setWhoami(w => w + value);
-            }
-
-        }, 100, frase[Symbol.iterator]());
-
-
-    }, []);
-
 
     return (
         <div className="welcome-section">
-            <h1 className="whoami">
-               {whoami}
-            </h1>
-            <div className="welcome-text">
-                <p>Application still under development, in the near future come again!</p>
-                <p>
+            <div className="welcome-wrapper">   
+                <div class="static-text">$ _</div>
+                <ul className="whoami">
+                        <li><span>whoami</span></li>
+                </ul>
+            </div>
+                <div className="welcome-text">
+                    <p>Application still under development, in the near future come again!</p>
+                    <p>
                     developer and linux system administration enthusiast. Learning Nodejs, Postgresql, Mongodb and React;
                     visit my github!
-                </p>
-            </div>
+                    </p>
+                </div>
+            
         </div>
     );
 }
