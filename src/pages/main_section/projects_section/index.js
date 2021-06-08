4ca785c1be139b18a18468159898905bd01df1d9
fabiokleis <fabiohkrc@gmail.com>
2021-06-07 21:16:00 -0300

    implemented arrow buttons

--- src/pages/main_section/projects_section/index.js
+++ src/pages/main_section/projects_section/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './index.css';
 import img from '../o_pior.png';
 import memory from '../gameOver.png';
-import LeftArrow from '../arrow_buttons/leftArrow';
-import RightArrow from '../arrow_buttons/rightArrow';
 
 export default function ProjectsSection(props){
-    
+    const [value, setValue] = useState(-400);
+
+    function toLeft(){
+        if(value !== 0) {
+            return setValue(400 + value);
+        };
+
+    }
+
+    function toRight(){
+        if(value > -800){
+            return setValue(-400 + value);
+        }
+    }
+
     return (
         <div className="projects-container">
-            <LeftArrow />
-            <div className="projects-section">
+            <div className="arrow-container-left">
+                <button className="arrow-left" onClick={toLeft}>{"<"}</button>
+            </div>
+            <div className="projects-section" style={{marginLeft: `${value}px` , marginRight: `${value}px`}}>
                 
                 <div className="projects-card">
                     
@@ -24,8 +38,16 @@ export default function ProjectsSection(props){
 
                 </div>
                 
+                <div className="projects-card">
+                    
+                    <img className="projects-img" src={memory} alt="project-overview" />
+
+                </div>
             </div>
-            <RightArrow />
+            <div className="arrow-container-right" onClick={toRight}>
+                <button className="arrow-right">{">"}</button>
+            </div>
+
         </div>
     );
 }
