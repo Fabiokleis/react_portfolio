246d255e020ae79cd4b6fd68fc29816e7e3ff60d
fabiokleis <fabiohkrc@gmail.com>
2021-06-09 18:23:58 -0300

    trying create a login and register forms

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -13,14 +13,14 @@ export default function Signin(props){
             
             <div className="signin-section">
             
-                <form className="signin-form">
+                <form method="POST" action="http://127.0.0.1:3001/users/login" className="signin-form">
                     
                     
                     <label className="signin-label">Email address</label>
                     <input name="email" id="email"className="signin-input" type="email"></input>
                     <label className="signin-label">Password</label>
                     <input name="password" id="password" className="signin-input" type="password"></input>
-                    <button className="signin-btn" type="">Sign in</button>
+                    <button className="signin-btn" type="submit">Sign in</button>
                 </form>
                 
             </div>
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -6,22 +6,24 @@ import Footer from '../../components/footer';
 
 export default function Signup(props){
 
+    function prevent(e){
+        e.preventDefault();
+        console.log(e);
+    }
     return (
         <>
             <Header show={false} />
             <h1 className="title-center">Create your account</h1>
             <div className="signup-section">
             
-                <form className="signup-form">
-                    
-                    
-                    <label htmlFor="name" className="signup-label">Username</label>
+                <form method="POST" action="http://127.0.0.1:3001/users" className="signup-form">
+                    <label  className="signup-label">Username</label>
                     <input name="name" id="name" className="signup-input" type="text"></input>
-                    <label htmlFor="email"  className="signup-label">Email address</label>
+                    <label  className="signup-label">Email address</label>
                     <input name="email" id="email"className="signup-input" type="email"></input>
-                    <label htmlFor="password" className="signup-label">Password</label>
+                    <label className="signup-label">Password</label>
                     <input name="password" id="password" className="signup-input" type="password"></input>
-                    <button className="signup-btn" type="">Sign up</button>
+                    <button onClick={prevent} className="signup-btn" type="submit">Sign up</button>
                 </form>
                 
             </div>
