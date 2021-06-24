8e61d66d05bfa80c02222391c833a57c6c9649cc
fabiokleis <fabiohkrc@gmail.com>
2021-06-23 22:15:14 -0300

    changed some styles and added a background image to app

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -13,17 +13,17 @@ export default function Footer(props){
             
             <ul className="footer-links">
                 <li className="footer-li">
-                    <a href="https://github.com/Fabiokleis" target="_blank"> 
+                    <a href="https://github.com/Fabiokleis" target="_blank" rel="noreferrer"> 
                         <img src={git} alt="github" title="github/Fabiokleis" />
                     </a>
                 </li>
                 <li className="footer-li">
-                    <a href="https://twitter.com/FabioKleis" target="_blank">
+                    <a href="https://twitter.com/FabioKleis" target="_blank" rel="noreferrer">
                         <img src={tt} alt="twitter" title="twitter/Fabiokleis" />
                     </a>
                 </li>
                 <li className="footer-li">
-                    <a href="https://instagram.com/fabiokleis" target="_blank">
+                    <a href="https://instagram.com/fabiokleis" target="_blank" rel="noreferrer">
                         <img src={insta} alt="instagram" title="instagram/fabiokleis" />
                     </a>
                 </li>
