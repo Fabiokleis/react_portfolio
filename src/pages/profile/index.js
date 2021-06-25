dd336366957d7837122b6e85f538fd03a5c4f019
fabiokleis <fabiohkrc@gmail.com>
2021-06-25 19:19:59 -0300

    created a update function to send put request

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -12,18 +12,21 @@ export default function Profile(props){
     const [msg, setMsg] = useState(null);
     const [flag, setFlag] = useState(false);
     const [posts, setPosts] = useState([]);
+    const [update, setUpdate] = useState(false);
+    const [postId, setPostId] = useState(null);
+
 
     useEffect(() => {
         getUserLastPosts(user.token, 'http://127.0.0.1:3001/posts/profile_posts?'+"page=1");
     }, [flag, user]);
 
-    async function createPost(e){
+    function createPost(e){
         e.preventDefault();
         const url = e.target.action;
         const title = e.target.elements[0].value;
         const description = e.target.elements[1].value;
         const data = {title, description};
-        const res = await createPostReq(data, user.token, url);
+        createPostReq(data, user.token, url);
         e.target.elements[0].value = "";
         e.target.elements[1].value = "";
         setFlag(false);
@@ -32,24 +35,59 @@ export default function Profile(props){
         }, 3000)
     }
 
-    async function updatePost(id){
-        
+    function updatePost(e){
+        e.preventDefault();
+        const title = e.target.elements[0].value;
+        const description = e.target.elements[1].value;
+        const data = {title, description};
+  
+        fetch(e.target.action+"?id="+postId,
+            {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': user.token
+                },
+                body: JSON.stringify(data)
+            }).then(res => res.json()).then(data => {
+                if(data.message){
+                    setMsg(data.message);
+                }else{
+                    setUpdate(false);
+                    setFlag(true);
+                }
+        }).catch(err => alert(err));
+
+        setFlag(false);
+        setTimeout(() => {
+            setMsg(null);
+        }, 3000);
+
     }
 
-    async function deletePost(id){
+    function swapUpdateState(id){
+        setUpdate(true);
+        setPostId(id);
+    }
+
+
+    function deletePost(id){
         fetch('http://127.0.0.1:3001/posts?id='+id, 
             {
                 headers: {'Authorization': user.token},
                 method: 'DELETE'
             }).then(res => res.json())
             .then(data => {
-                setFlag(true);
                 if(data.message){
                     setMsg(data.message);
                 }else{
                     setFlag(true);               
                 }
             }).catch(err => alert('error on server! try again later...'));
+        setFlag(false);
+        setTimeout(() => {
+            setMsg(null);
+        }, 3000);
     }
 
     function getUserLastPosts(jwt, url){
@@ -67,7 +105,7 @@ export default function Profile(props){
     }
 
     function createPostReq(data, jwt, url){
-        const res = fetch(url, 
+        fetch(url, 
             {
                 headers: {
                     'Content-Type': 'application/json',
@@ -86,7 +124,6 @@ export default function Profile(props){
             }
          }).catch(err => alert('error on servers! try again later...'));
 
-        return res;
     }
 
 
@@ -100,7 +137,7 @@ export default function Profile(props){
                 </div>
                 <div className="user-container">
                     <div className="img-container">
-                        <img className="user-img" src="" alt="profile picture" />
+                        <img className="user-img" src="" alt="profile" />
                     </div>
                     <div className="user-credentials">
                         <h3 className="user-name">@{user.name}</h3>
@@ -114,23 +151,34 @@ export default function Profile(props){
                     <form method="POST" action="http://127.0.0.1:3001/posts" onSubmit={createPost} id="main-form"> 
                         <div className="newpost-container">
                             <input placeholder="title" required name="title" id="title" type="textarea" />
-                            <textarea placeholder="description, max char 255" maxLength="255" required name="description" id="description" form="main-form"/>
+                            <textarea placeholder="description, max char 255" maxLength="255" required name="description" className="description" form="main-form"/>
                             <button className="post-btn" type="submit"><img src={add} alt="check" /></button> 
                         </div>
                     </form>
+                    <form className={update?"":"hidden"} action='http://127.0.0.1:3001/posts' id="update-form" onSubmit={(e) => updatePost(e)}>
+                         <div className="newpost-container">
+                             <input placeholder="update title" required name="title" id="title" type="textarea" />
+                             <textarea placeholder="update description, max char 255" maxLength="255" 
+                             required name="description" className="description" form="update-form"/>               
+                             <button className="post-btn" type="submit"><img src={add} alt="check" /></button> 
+                         </div>
+                    </form>
+
                     <div className="user-last-posts">
                       {posts.map(post => (
+                  
                         <div className="user-post" key={post.id} >
+                        <div className="user-post-header">
 
-                        <div className="user-post-header">  
                           <div className="user-post-name">
                             {"@"+post.name} 
                           </div>
+
                           <div className="user-post-title">
-                            {post.title}
-                          </div>
+                               {post.title}
+                          </div> 
                           <div className="user-post-date">
-                            {new Date(post.updated_at).toLocaleDateString('en-US',{
+                            {new Date(post.updated_at).toLocaleDateString('en-US', {
                                 weekday: 'long',
                                 month: 'long',
                                 day:'numeric',
@@ -138,15 +186,15 @@ export default function Profile(props){
                                 minute: 'numeric'}
                             )}
                           </div>
-                        </div>
-                        <div className="user-post-desc">
-                          {post.description}
-                        </div>
+                          </div>
+                          <div className="user-post-desc">
+                            {post.description}
+                         </div>
                         <div className="user-post-reqs">
-                            <div className="update-btn" onClick={() => updatePost(post.id)}>
+                            <div className={!update?"update-btn":"hidden"} onClick={() => swapUpdateState(post.id)}>
                                 <img src={edit} alt="edit" />
                             </div>
-                            <div className="delete-btn" onClick={() => deletePost(post.id)}>
+                           <div className={!update?"delete-btn":"hidden"} onClick={() => deletePost(post.id)}>
                                 <img src={del} alt="delete" />
                             </div>
                         </div>
